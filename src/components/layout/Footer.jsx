2b25af9b85d6ef5699dee58b99f51636e1fceaee
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-11-15 16:24:03 +0000

    calculador-preco-de-projeto

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,14 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+
 export default function Footer (){
     return(
-<h1></h1>
+        <div className='bg-zinc-900 grid h-[155px] py-7 justify-center items-center '>
+        <div className='flex justify-center gap-5 text-white'>
+            <FaFacebook className='hover:text-yellow-400'/>
+            <FaInstagram className='hover:text-yellow-400'/>
+            <FaLinkedin className='hover:text-yellow-400'/>
+        </div>
+        <p className='text-white text-sm'><span className='text-yellow-400 font-bold'>Costs</span> &copy; 2024 </p>
+        </div>
     )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,12 @@
+import savings from '../../img/savings.png'
+
 export default function Home (){
     return(
-        <div>
-            <h1>Home</h1>
-        </div>
+        <section className='grid justify-items-center items-center my-[88px]'>
+            <h1 className='sm:text-[40px] font-bold'>Bem-vindo ao <span className='text-yellow-400 bg-zinc-900 px-1'>Costs</span></h1>
+            <p className='text-zinc-500 my-6'>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <a href="/" className='underline text-purple-800 hover:text-purple-600'>Criar Projeto</a>
+            <img className='sm:w-[400px]' src={savings} alt="Costs" />
+        </section>
     )
 }
